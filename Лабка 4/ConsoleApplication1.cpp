19999fb0764838231a3d4129caf72be549eb5e35
KuzBoBicH <91948842+KuzBoBicH@users.noreply.github.com>
2022-10-30 19:24:15 +0200

    Update ConsoleApplication1.cpp

--- Лабка 4/ConsoleApplication1.cpp
+++ Лабка 4/ConsoleApplication1.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cstdlib>
 #include <Windows.h>
 using namespace std;
@@ -7,52 +7,39 @@ int i;
 class Array {
     float* m;
     int n;
-    
+
 public:
-    Array(int k) {
-        cout << "Constr z param" << endl;
-        n = k;
+    int all = 1;
+
+    Array() {
+        n = 8;
         m = new float[n];
         set_data();
     }
     ~Array() {
         delete m;
-        cout << "\nDestruktor." << endl;
     }
     void set_data() {
-        for (i = 0; i < n; i++) {
+        for (i = 0; i <= n; i++) {
             if (i % 2 == 0) {
                 m[i] = i - 4.0;
             }
             else {
-                m[i] = i + 6.0;
+                m[i] = i - 6.0;
+                all = all *m[i];
+                
             }
         }
-        output();
-    }
-    void output() {
-        cout << "KIlkist dodatnih:" << endl;
-        for (i = 0; i < n; i++) {
-            cout << m[i] << " ";
-        }
+        cout << all << "\n";
+       
     }
 
-    friend void sum(Array &obj);
+ 
 };
-void sum(Array &obj) {
-    float sum = 0.0;
-    cout << "\nDodatni element: " << obj.m << "\n";
-    for (i = 0; i < obj.n; i++) {
-        if (!(i % 2) == 0) {
-            cout << obj.m[i] << " ";
-            sum = sum + obj.m[i];
-        }
-    } cout << endl;
-    cout << "Summa dodatnih elementiv: " << sum;
-}
+
 int main() {
-    Array arr(8);
-    sum(arr);
+    Array();
+    Array set_data();
     system("pause");
     return 0;
 }
